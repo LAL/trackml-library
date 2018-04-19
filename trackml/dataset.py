c4271f49455a38563af6d1b96c72e4205923c6cf
Moritz Kiehn <msmk@cern.ch>
2018-04-19 11:54:38 +0200

    dataset: extend docstrings

--- trackml/dataset.py
+++ trackml/dataset.py
@@ -46,38 +46,34 @@ TRUTH_DTYPES = dict([
 ])
 
 def _load_event_data(prefix, name, dtype):
+    """Load per-event data for one single type, e.g. hits, or particles.
     """
-    Load per-event data for one single type, e.g. hits, or particles.
-    """
-    files = glob.glob('{}-{}.csv*'.format(prefix, name))
+    expr = '{!s}-{}.csv*'.format(prefix, name)
+    files = glob.glob(expr)
     if len(files) == 1:
         return pandas.read_csv(files[0], header=0, index_col=False, dtype=dtype)
     elif len(files) == 0:
-        raise Exception('No file found matching {}-{}.csv*'.format(prefix, name))
+        raise Exception('No file matches \'{}\''.format(expr))
     else:
-        raise Exception('More than one file found matching {}-{}.csv*'.format(prefix, name))
+        raise Exception('More than one file matches \'{}\''.format(expr))
 
 def load_event_hits(prefix):
-    """
-    Load the hits information for a single event with the given prefix.
+    """Load the hits information for a single event with the given prefix.
     """
     return _load_event_data(prefix, 'hits', HITS_DTYPES)
 
 def load_event_cells(prefix):
-    """
-    Load the hit cells information for a single event with the given prefix.
+    """Load the hit cells information for a single event with the given prefix.
     """
     return _load_event_data(prefix, 'cells', CELLS_DTYPES)
 
 def load_event_particles(prefix):
-    """
-    Load the particles information for a single event with the given prefix.
+    """Load the particles information for a single event with the given prefix.
     """
     return _load_event_data(prefix, 'particles', PARTICLES_DTYPES)
 
 def load_event_truth(prefix):
-    """
-    Load only the truth information for a single event with the given prefix.
+    """Load only the truth information for a single event with the given prefix.
     """
     return _load_event_data(prefix, 'truth', TRUTH_DTYPES)
 
@@ -88,22 +84,44 @@ _LOAD_FUNCTIONS = {
     'truth': load_event_truth, }
 
 def load_event(prefix, parts=['hits', 'cells', 'particles', 'truth']):
-    """
-    Load data for a single event with the given prefix.
+    """Load data for a single event with the given prefix.
 
-    With the default settings a tuple (hits, particles, truth) is returned.
-    Each element is a pandas DataFrame with field names identical to the CSV
-    column names and appropriate types. Depending on the options only a
-    subset of the DataFrames is loaded.
+    Parameters
+    ----------
+    prefix : str or pathlib.Path
+        The common prefix name for the event files, i.e. without `-hits.csv`).
+    parts : List[{'hits', 'cells', 'particles', 'truth'}], optional
+        Which parts of the event files to load.
+
+    Returns
+    -------
+    tuple
+        Contains a `pandas.DataFrame` for each element of `parts`. Each
+        element has field names identical to the CSV column names with
+        appropriate types.
     """
     return tuple(_LOAD_FUNCTIONS[_](prefix) for _ in parts)
 
 def load_dataset(path, skip=None, nevents=None, **kw):
-    """
-    Provide an iterator over (all) events in a dataset directory.
+    """Provide an iterator over (all) events in a dataset directory.
 
-    For each event it returns the event_id and the output of `load_event`
-    concatenated as a single tuple.
+    Parameters
+    ----------
+    path : str or pathlib.Path
+        Path to the dataset directory.
+    skip : int, optional
+        Skip the first `skip` events.
+    nevents : int, optional
+        Only load a maximum of `nevents` events.
+    parts : List[{'hits', 'cells', 'particles', 'truth'}], optional
+        Which parts of each event files to load.
+
+    Yields
+    ------
+    event_id : int
+        The event identifier.
+    *data
+        Event data element as specified in `parts`.
     """
     files = glob.glob(op.join(path, 'event*-*'))
     names = set(op.basename(_).split('-', 1)[0] for _ in files)
