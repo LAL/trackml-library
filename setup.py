98774ce5df48f3dae19d6c3b9aaebec1516f590b
Moritz Kiehn <msmk@cern.ch>
2018-06-18 16:48:58 +0200

    require pandas 0.21.0 or higher

--- setup.py
+++ setup.py
@@ -32,6 +32,9 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     packages=['trackml'],
-    install_requires=['numpy', 'pandas'],
+    install_requires=[
+        'numpy',
+        'pandas>=0.21.0',
+    ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
 )
