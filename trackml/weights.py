d69f6f73866c3ab69e25978aacf570d250662311
Moritz Kiehn <msmk@cern.ch>
2018-09-03 12:31:10 +0200

    add hit weights calculations for phase2

--- trackml/weights.py
+++ trackml/weights.py
@@ -9,6 +9,8 @@ import math
 import numpy
 import pandas
 
+from .utils import decode_particle_id
+
 def _compute_order_weight_matrix(proposal, min_hits, max_hits):
     """Compute the hit order weight matrix.
 
@@ -122,6 +124,60 @@ def weight_hits_phase1(truth, particles):
     w = combined['weight_pt'] * combined['weight_order']
     w /= w.sum()
     combined['weight'] = w
+
+    # return w/o intermediate columns
+    return combined.drop(columns=['particle_vz', 'abs_dvz'])
+
+def weight_hits_phase2(truth, particles):
+    """Compute per-hit weights for the phase 2 scoring metric.
+
+    This is the phase 1 metric with an additional particle preselection, i.e.
+    only a subset of the particles have a non-zero score.
+
+    Parameters
+    ----------
+    truth : pandas.DataFrame
+        Truth information. Must have hit_id, particle_id, and tz columns.
+    particles : pandas.DataFrame
+        Particle information. Must have particle_id, vz, px, py, and nhits
+        columns.
+
+    Returns
+    -------
+    pandas.DataFrame
+        `truth` augmented with additional columns: particle_nhits, ihit,
+        weight_order, weight_pt, and weight.
+    """
+    # fill selected per-particle information for each hit
+    selected = pandas.DataFrame({
+        'particle_id': particles['particle_id'],
+        'particle_vz': particles['vz'],
+        'particle_nhits': particles['nhits'],
+        'weight_pt': weight_pt(numpy.hypot(particles['px'], particles['py'])),
+    })
+    selected = decode_particle_id(selected)
+    combined = pandas.merge(truth, selected,
+                            how='left', on=['particle_id'],
+                            validate='many_to_one')
+
+    # fix pt weight for hits w/o associated particle
+    combined['weight_pt'].fillna(0.0, inplace=True)
+    # fix nhits for hits w/o associated particle
+    combined['particle_nhits'].fillna(0.0, inplace=True)
+    combined['particle_nhits'] = combined['particle_nhits'].astype('i4')
+
+    # compute hit count and order using absolute distance from particle vertex
+    combined['abs_dvz'] = numpy.absolute(combined['tz'] - combined['particle_vz'])
+    combined['ihit'] = combined.groupby('particle_id')['abs_dvz'].rank().transform(lambda x: x - 1).fillna(0.0).astype('i4')
+    # compute order-dependent weight
+    combined['weight_order'] = combined[['ihit', 'particle_nhits']].apply(weight_order, axis=1)
+
+    # compute normalized combined weight w/ extra particle selection
+    weight = combined['weight_pt'] * combined['weight_order']
+    weight[combined['generation'] != 0] = 0
+    weight /= weight.sum()
+    # normalize total event weight
+    combined['weight'] = weight
 
     # return w/o intermediate columns
     return combined.drop(columns=['particle_vz', 'abs_dvz'])
